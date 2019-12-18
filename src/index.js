8c640a3e19a4ee2d8775ab974dbb919be8451c90
Sanjay <sgupt9999@gmail.com>
2019-12-17 21:15:30 -0600

    Installed two new packages https and fs. Made changes to index.js to listen on port 443

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 require('./db/mongoose'); // This will make sure that the entire file runs
 const userRouter = require('./routers/user');
@@ -16,12 +18,19 @@ app.use(cookieParser());
 app.use(userRouter);
 app.use(likesRouter);
 
+/* Commenting this out, as a different method with letsencrypt
 app.listen(port, () => {
 	console.log('Express server is running');
 });
+*/
 
-
-
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/aaliya-gallery.com/privkey1.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/aaliya-gallery.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/aaliya-gallery.com/chain1.pem'),
+}, app).listen(443, () => {
+  console.log('Listening...')
+})
 
 
 
