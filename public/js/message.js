27fdb36019e6a15dc528c00e8327c946bfa976db
Sanjay <sgupt9999@gmail.com>
2020-01-09 12:50:25 -0600

    fixing a bug in message.js

--- public/js/message.js
+++ public/js/message.js
@@ -24,9 +24,9 @@ const getUserInfo = async () => {
 const initializeForm = async () => {
 // If user is logged in, then add that information to form so the user doest have to type it again
     const user = await getUserInfo();
-    if (user.user) {
-        $messageName.value = user.user.name;
-        $messageEmail.value = user.user.email
+    if (user) {
+        $messageName.value = user.name;
+        $messageEmail.value = user.email
         $messageMessage.focus();
     }
     else {
