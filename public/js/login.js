480cb09b9532f3d59c0ddf20c65e65f5c705ba1d
Sanjay <sgupt9999@gmail.com>
2020-04-11 11:42:00 -0500

    The previous change didnt work and the artist register page is too long and on mobile device some fields get hidden. Added some code based on https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent to detect if it's a mobile device. Also on the login.html made login field as autofocus and removed the register buttons from a form

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,43 @@ $errorLogin = document.getElementById('error-login');
 $loginResendEmail = document.getElementById('login-resend-email');
 $btnLoginResendEmail = document.getElementById('btn-login-resend-email');
 
-document.getElementById('loginid').focus();
+let hasTouchScreen = false;
+if ("maxTouchPoints" in navigator) { 
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0; 
+} else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
+
+console.log(hasTouchScreen);
+
+//document.getElementById('loginid').focus();
+document.getElementById('artist-account').addEventListener("click", () => {
+    const height = screen.height;
+    console.log(height);
+    const availHeight = screen.availHeight;
+    console.log(availHeight);
+    const innerHeight = window.innerHeight;
+    console.log(innerHeight);
+    //window.location.href='/register-artist.html'
+})
+document.getElementById('browser-account').addEventListener("click", () => {
+    window.location.href='/register.html'
+})
+
 
 document.forms['login'].addEventListener('submit', async (event) => {
 // When the login form is successfully submitted, render the header of the home page with the correct 
